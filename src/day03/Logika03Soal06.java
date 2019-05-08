9b098ff70bb0f1801ff118080d0c94d7f01f70f8
arfanhari <arfan.angga@gmail.com>
2019-05-08 16:23:00 +0700

    no message

--- src/day03/Logika03Soal06.java
+++ src/day03/Logika03Soal06.java
@@ -1,12 +1,54 @@
 package day03;
 
+import java.util.Scanner;
+
 public class Logika03Soal06 {
+	protected static Scanner input;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		input = new Scanner(System.in);
+		System.out.println("Masukkan N :");
+		int n = input.nextInt();
+		System.out.println("Masukkan M : ");
+		int m = input.nextInt();
+
+		deretAngka(n);
+		int array[] = deretAngka(n);
+		int array2[] = deretAngka(n);
+		for (int i = 0; i <= m; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(array[j] + "\t");
+			}
+			System.out.println();
+
+			for (int k = n - 1; k >= 0; k--) {
+				if (k == n - 1) {
+					array[k] = array2[0];
+				} else {
+					array[k] = array2[k + 1];
+				}
+			}
+
+			for (int l = 0; l < n; l++) {
+				array2[l] = array[l];
+			}
+
+		}
 
 	}
 
-	//awal2 = awal2 + (2 * (i + 1));
+	static int[] deretAngka(int n) {
+		int result[] = new int[n];
+		int awal = 2;
+		int tambah = 2;
+		for (int i = 0; i < n; i++) {
+			result[i] = awal;
+			awal = awal + tambah;
+			tambah += 2;
+
+		}
+
+		return result;
+	}
 
 }
