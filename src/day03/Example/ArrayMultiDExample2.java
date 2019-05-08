45b51bd1eebba7500ffb113a2d0518a7297a6a0e
arfanhari <arfan.angga@gmail.com>
2019-05-08 21:55:26 +0700

    no message

---
+++ src/day03/Example/ArrayMultiDExample2.java
@@ -0,0 +1,28 @@
+package day03.Example;
+
+public class ArrayMultiDExample2 {
+	
+	public static void main(String[] args) {
+		
+		// declaring a 2D array with odd columns
+		int arr[][] = new int[3][];
+		arr[0] = new int[3];
+		arr[1] = new int[4];
+		arr[2] = new int[2];
+		
+		// Inisialisasi array
+		int count = 0;
+		
+		for (int i = 0; i < arr.length; i++)
+			for (int j = 0; j < arr[i].length; j++)
+				arr[i][j] = count++;
+
+		// Cetak data
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
